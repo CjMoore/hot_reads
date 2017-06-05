adb1c8f0fdecd21c967bca2bc5d17932aae70a72
Charlotte Moore <mooc123@gmail.com>
2017-06-05 17:39:29 -0600

    added model test on hot_read for get_recent method

--- spec/models/hot_read_spec.rb
+++ spec/models/hot_read_spec.rb
@@ -4,4 +4,20 @@ RSpec.describe HotRead, type: :model do
   context 'validations' do
     it { should validate_presence_of(:url) }
   end
+
+  context 'methods' do
+    it "returns recently read links only" do
+      hotread1 = HotRead.create(url: 'https://google.com', count: 3, updated_at: 3.days.ago)
+      hotread2 = HotRead.create(url: 'https://reddit.com', count: 2)
+      hotread3 = HotRead.create(url: 'https://gmail.com', count: 1)
+      hotread4 = HotRead.create(url: 'https://four.com', count: 1)
+      hotread5 = HotRead.create(url: 'https://five.com', count: 1)
+
+      hot_reads = HotRead.get_recent
+
+      expect(hot_reads.count).to eq(4)
+      expect(hot_reads[0].url).to eq('https://reddit.com')
+      expect(hot_reads[0].count).to eq(2)
+    end
+  end
 end
