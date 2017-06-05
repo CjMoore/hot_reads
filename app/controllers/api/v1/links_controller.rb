7815df4480a2c6aadebf4886f8b149a5ef48afc2
Charlotte Moore <mooc123@gmail.com>
2017-06-05 14:50:37 -0600

    hotread endpoint returns hot reads

--- app/controllers/api/v1/links_controller.rb
+++ app/controllers/api/v1/links_controller.rb
@@ -5,11 +5,11 @@ class Api::V1::LinksController < ApplicationController
     @link = HotRead.find_by(url: params['url'])
     if @link.nil?
       @new_hot_read =  HotRead.create(url: params['url'], count: 1)
-      render status: 201, json: @new_hot_read
+      render status: 201, json: HotRead.get_recent
     else
       new_count = @link.count + 1
       @link.update_attribute(:count, new_count)
-      render status: 200, json: @link
+      render status: 200, json: HotRead.get_recent
     end
   end
 end
