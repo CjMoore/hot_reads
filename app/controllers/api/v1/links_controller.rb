105b62c4e88b7f7341287fb6337e1480a08aee8e
Charlotte Moore <mooc123@gmail.com>
2017-06-05 13:22:32 -0600

    accepts hot read from urlockbox and displays top read

--- app/controllers/api/v1/links_controller.rb
+++ app/controllers/api/v1/links_controller.rb
@@ -4,12 +4,12 @@ class Api::V1::LinksController < ApplicationController
   def create
     link = HotRead.find_by(url: params['url'])
     if link.nil?
-      HotRead.create(url: params['url'], count: 1)
-      render status: 201
+      new_hot_read =  HotRead.create(url: params['url'], count: 1)
+      render status: 201, json: new_hot_read
     else
       new_count = link.count + 1
       link.update_attribute(:count, new_count)
-      render status: 200
+      render status: 200, json: link
     end
   end
 end
